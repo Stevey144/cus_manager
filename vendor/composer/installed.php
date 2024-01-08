b55ab825936e8e185bd6ad39753a197ce264a66c
stevy <45201174+Stevey144@users.noreply.github.com>
2024-01-08 18:44:20 +0000

    updated

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2ea7f58c1f4653b2d5e20c8786cdd20ee7af5482',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2ea7f58c1f4653b2d5e20c8786cdd20ee7af5482',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
